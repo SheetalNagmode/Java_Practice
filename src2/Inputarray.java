7df0efa5a558bf6f0cb66a43f8eef391851a0874
SheetalNagmode <sheetalnagmode@gmail.com>
2024-02-02 09:31:11 -0500

    2nd Feb

--- src2/Inputarray.java
+++ src2/Inputarray.java
@@ -1,10 +1,13 @@
 package src2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Inputarray {
 
     public static void main(String[] args) {
+
+        // Array of primitives
 
         Scanner in = new Scanner(System.in);
 
@@ -16,12 +19,20 @@ public class Inputarray {
         arr[4] = 555;
         System.out.println(arr[3]);
 
+
+
+
+
         // input using for loops
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = in.nextInt();
 
         }
+        // Another way to print using the Array class:
+
+        System.out.println(Arrays.toString(arr));
+
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -34,6 +45,17 @@ public class Inputarray {
             }
 
         //     System.out.println(arr[5]);  // index out of bound error
+
+        // Array of objects
+
+            String[] str = new String[4];
+        for (int i = 0; i < str.length; i++) {
+            str[i] = in.next();
+        }
+        System.out.println(Arrays.toString(str));
+
+
+
 
     }
 }
