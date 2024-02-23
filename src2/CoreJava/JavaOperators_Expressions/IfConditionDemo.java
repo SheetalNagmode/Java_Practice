75d328ecff8728f9432520d2c5b2e44019e9f14e
SheetalNagmode <sheetalnagmode@gmail.com>
2024-02-23 16:50:54 -0500

    23rd Feb 2024

---
+++ src2/CoreJava/JavaOperators_Expressions/IfConditionDemo.java
@@ -0,0 +1,23 @@
+package src2.CoreJava.JavaOperators_Expressions;
+
+public class IfConditionDemo {
+    public static void main(String[] args) {
+
+        // if condition:
+        // syntax:
+        // if(condition)
+        // {
+        //  statements;
+        //   }
+
+        int person_age = 25;
+
+        if (person_age >= 18) {
+            System.out.println("Eligible for vote");
+
+        }
+
+        // we haven't specified what if the condition is false
+
+    }
+}
