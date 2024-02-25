8da0398c9072cc573093548c36043c1ee76ae085
SheetalNagmode <sheetalnagmode@gmail.com>
2024-02-25 14:56:50 -0500

    25th Feb 2024

--- src2/CoreJava/ConditionalStatements/MultipleStatements.java
+++ src2/CoreJava/ConditionalStatements/MultipleStatements.java
@@ -3,6 +3,33 @@ package src2.CoreJava.ConditionalStatements;
 public class MultipleStatements {
     public static void main(String[] args) {
 
+        // If condition requires a boolean value
+
+        if (true) {        // we can directly put boolean value instead of expression
+            System.out.println(1);
+        }else{
+            System.out.println(2);
+        }
+
+        if (1==2) {       // expression = boolean value true or false
+            System.out.println(1);
+        }else{
+            System.out.println(2);
+        }
+
+        // Nested if condition:
+        // One if condition.. have multiple if else.. conditions
+
+        if(true){
+            if(false){
+                System.out.println("abc");
+            }else{
+                System.out.println("xyz");
+            }
+        }else{
+            System.out.println(123);
+        }
+
 
     }
 }
