f22519cc301febc012fce5af25debb1309a277b1
SheetalNagmode <sheetalnagmode@gmail.com>
2024-02-28 17:06:22 -0500

    28th Feb 2024

---
+++ src2/CoreJava/Loops_JumpStatements/WhileLoopDemo.java
@@ -0,0 +1,26 @@
+package src2.CoreJava.Loops_JumpStatements;
+
+public class WhileLoopDemo {
+    public static void main(String[] args) {
+
+        // While Loop Statement
+        /* Syntax:
+            initialization;
+            while(condition){
+            statements;
+            increment/ decrement;
+            }
+         */
+
+        // Example 1: Print 1 to 10 numbers using while loop.
+
+        int i =1;          // initialization
+        while(i<=10){       //  condition
+            System.out.println(i);
+            i++;             // incrementation
+        }
+
+
+
+    }
+}
