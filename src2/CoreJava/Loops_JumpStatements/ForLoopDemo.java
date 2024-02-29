5923fbc1079d8da7570f72be0d3c8238da240339
SheetalNagmode <sheetalnagmode@gmail.com>
2024-02-29 13:40:05 -0500

    29th Feb 2024

---
+++ src2/CoreJava/Loops_JumpStatements/ForLoopDemo.java
@@ -0,0 +1,44 @@
+package src2.CoreJava.Loops_JumpStatements;
+
+public class ForLoopDemo {
+    public static void main(String[] args) {
+
+        // For loop:
+        /*Syntax:
+        for (initialization; condition; increment/decrement)
+        {
+        statements;
+        }
+         */
+
+        // Print 1 to 10 numbers
+
+        for (int i =1; i <=10; i++)
+        {
+            System.out.println(i);
+        }
+
+        // Print even numbers 1 to 10
+
+        for(int a=2; a<=10; a+=2)
+        {
+            System.out.println(a);
+        }
+
+        // Print even and odd numbers from 1 to 10:
+
+        for (int x = 0; x<=10; x++)
+        {
+            if (x%2==0){
+                System.out.println(x + "-"+ "Even");
+            }else{
+                System.out.println(x + "-"+ "Odd");
+            }
+        }
+
+
+
+
+
+    }
+}
