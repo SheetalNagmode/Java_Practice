6262f9fa044daa50dab1f849f8db4dcd23c93104
SheetalNagmode <sheetalnagmode@gmail.com>
2024-03-26 15:02:38 -0400

    26th Mar 2024

--- src2/CoreJava/Arrays/TwoDimensionalArray.java
+++ src2/CoreJava/Arrays/TwoDimensionalArray.java
@@ -31,14 +31,14 @@ public class TwoDimensionalArray {
 
         // Approach 2:
 
-        int a[][]= {{100, 200},
+        int b[][]= {{100, 200},
                     {300, 400},
                     {500, 600}
                 };
 
         // Find size of an array
-        System.out.println("Length of rows "+ a.length);
-        System.out.println("Length of columns :" + a[0].length);
+        System.out.println("Length of rows "+ b.length);
+        System.out.println("Length of columns :" + b[0].length);
 
         // read single value from an array
         System.out.println(a[2][1]);   //600
